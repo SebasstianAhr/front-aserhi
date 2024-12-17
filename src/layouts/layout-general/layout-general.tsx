5b257b495ad7234de14534146e7d7fa6e5f5d308
SebasstianAhr <sebasstianahr117@gmail.com>
2024-12-17 12:12:03 -0500

    I add styles responsib to app

--- src/layouts/layout-general/layout-general.tsx
+++ src/layouts/layout-general/layout-general.tsx
@@ -1,24 +1,36 @@
-import Sidebar from "../../components/sidebar/sidebar"
-import Navbar from "../../components/navbar/navbar"
-import './layout-general.css'
-
+import { useState } from "react";
+import Sidebar from "../../components/sidebar/sidebar";
+import Navbar from "../../components/navbar/navbar";
+import BurgerMenu from "../../components/burger-menu/burger-menu";
+import "./layout-general.css";
 
 interface ChildrenProps {
-    children: JSX.Element
+    children: JSX.Element;
 }
 
-const LayoutGeneral = ({children}: ChildrenProps): JSX.Element => {
+const LayoutGeneral = ({ children }: ChildrenProps): JSX.Element => {
+    const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarVisible(!isSidebarVisible);
+    };
+
     return (
         <div className="layout__general">
-            <div className="layout__sidebar-part">
-                <Sidebar />
-            </div>
+            <BurgerMenu onClick={toggleSidebar} />
+            {isSidebarVisible && (
+                <div className="layout__sidebar-part">
+                    <div className="layout__sidebar-menu">
+                        <Sidebar />
+                    </div>
+                </div>
+            )}
             <div className="layout__next-content">
                 <Navbar />
                 <div className="layout__principal-content">{children}</div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default LayoutGeneral
+export default LayoutGeneral;
