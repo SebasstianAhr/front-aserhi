25652e8fd608ca3d891bdf5225e465e5ddf21bf5
SebasstianAhr <sebasstianahr117@gmail.com>
2025-01-27 21:44:06 -0500

    I add styles of section about

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -82,12 +82,36 @@ const LandingPage = () => {
         </section>
         <section id="about" className="about">
           <div className='about__content about__us'>
-            <h2 className='about__title'>title</h2>
+            <h2 className='about__title'>Sobre Nosotros</h2>
             <p className='about__paragrapht'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit soluta facilis placeat expedita mollitia nisi necessitatibus ab doloremque eaque ea debitis voluptatum quia saepe asperiores ducimus voluptates vitae, accusantium harum.</p>
           </div>
-          <div className='about__content about__content--margin'>
-            <h2 className='about__title'>title</h2>
-            <p className='about__paragrapht'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit soluta facilis placeat expedita mollitia nisi necessitatibus ab doloremque eaque ea debitis voluptatum quia saepe asperiores ducimus voluptates vitae, accusantium harum.</p>
+          <div className='about__content about__content--margin about__content--flex'>
+            <div className="abaut-content__card">
+              <div className="card__content">
+                <div className="icon__container">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="mountain-sun" width={75} height={75} className="icon__card svg-inline--fa fa-mountain-sun text-7xl" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M560 160A80 80 0 1 0 560 0a80 80 0 1 0 0 160zM55.9 512H381.1h75H578.9c33.8 0 61.1-27.4 61.1-61.1c0-11.2-3.1-22.2-8.9-31.8l-132-216.3C495 196.1 487.8 192 480 192s-15 4.1-19.1 10.7l-48.2 79L286.8 81c-6.6-10.6-18.3-17-30.8-17s-24.1 6.4-30.8 17L8.6 426.4C3 435.3 0 445.6 0 456.1C0 487 25 512 55.9 512z"></path></svg>
+                </div>
+                <div className="card__content--text">
+                  <h5 className="card__content--title">Misión</h5>
+                  <p className="card__content--description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure numquam voluptatum, nisi quas sapiente dolorum possimus modi! Quae itaque reprehenderit in veritatis impedit, non quis ab, temporibus fuga, ullam magnam!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="abaut-content__card ">
+              <div className="card__content">
+                <div className="icon__container">
+                <svg aria-hidden="true" width={75} height={75} focusable="false" data-prefix="fas" data-icon="bullseye" className="svg-inline--fa fa-bullseye text-7xl" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"></path></svg>
+                </div>
+                <div className="card__content--text">
+                  <h5 className="card__content--title">Misión</h5>
+                  <p className="card__content--description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure numquam voluptatum, nisi quas sapiente dolorum possimus modi! Quae itaque reprehenderit in veritatis impedit, non quis ab, temporibus fuga, ullam magnam!
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='about__content about__content--margin'>
             <h2 className='about__title'>title</h2>
